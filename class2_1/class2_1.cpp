3bcb8ae279fe0c38017ea24dbe2b439b09eea1f5
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-03-06 14:29:06 +0800

    2-1

--- class2_1/class2_1.cpp
+++ class2_1/class2_1.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
+	cv::Mat src_color = imread("D:\\testImage\\solar_cell_01.png");
+	std::vector<cv::Mat> channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red", R);
+	cv::imshow("blue", B);
+	cv::imshow("green", G);
+	cv::imshow("original Mat", src_color);
+	waitKey(0);
+
 }
+
 
